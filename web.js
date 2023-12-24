fd0c8e5739efdfadb12be1880dbf1d4e79d0d249
toolsapkpro <145754947+toolsapkpro@users.noreply.github.com>
2023-12-24 07:15:28 -0800

    Add files via upload

---
+++ web.js
@@ -0,0 +1,18 @@
+//window.confirm("Welcome Wizard");
+//function showDialog(message) {
+    //var dialogOverlay = document.getElementById("dialog-overlay");
+    //var dialogBox = document.getElementById("dialog-box");
+    //var dialogMessage = document.getElementById("dialog-message");
+    //var dialogButton = document.getElementById("dialog-button");
+  
+    //dialogMessage.innerHTML = message;
+  
+    //dialogOverlay.style.display = "block";
+    //dialogBox.style.display = "block";
+  
+    //dialogButton.onclick = function() {
+    //dialogOverlay.style.display = "none";
+    //dialogBox.style.display = "none";
+    //}
+//}
+//showDialog("Your message goes here");
